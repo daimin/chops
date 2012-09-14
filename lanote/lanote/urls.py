ac18b771843e47e87e5799f56af2f51a59826bfe
daimin <vagasnail@gmail.com>
2012-09-14 18:22:45 +0800

    lanote restart.

--- lanote/lanote/urls.py
+++ lanote/lanote/urls.py
@@ -12,9 +12,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     
-    url( r'^$/(?P<path>.*)$', 'view.PageWithJquery'
-    ),
-    
     url( r'^js/(?P<path>.*)$', 'django.views.static.serve',
             { 'document_root': './js/' }
     ),
--- lanote/lanote/view.py
+++ lanote/lanote/view.py
@@ -1,7 +1,4 @@
-"""
-"""
-from django.shortcuts import render_to_response
- 
-def PageWithJquery( request ):
-    return render_to_response( 'index.html',
-            {"mytitle":"customize_title"})
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse("Hello world")
