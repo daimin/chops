d98ebec6001e31fb13c8aaf17bb7005bd073998c
daimin <vagasnail@gmail.com>
2012-11-28 00:20:16 +0800

    add img

--- PrinterStat/gen/com/belstar/printerstat/R.java
+++ PrinterStat/gen/com/belstar/printerstat/R.java
@@ -17,31 +17,34 @@ public final class R {
     public static final class drawable {
         public static final int bg=0x7f020000;
         public static final int bg_hui=0x7f020001;
-        public static final int black=0x7f020006;
-        public static final int blue=0x7f020007;
-        public static final int brown=0x7f02000b;
+        public static final int black=0x7f02000b;
+        public static final int blue=0x7f02000c;
+        public static final int brown=0x7f020010;
         public static final int btn_normal=0x7f020002;
         public static final int btn_pressed=0x7f020003;
-        public static final int green=0x7f02000c;
-        public static final int ic_launcher=0x7f020004;
-        public static final int mblue=0x7f02000a;
-        public static final int mi=0x7f020008;
-        public static final int sblue=0x7f020009;
-        public static final int tgreen=0x7f02000d;
-        public static final int white=0x7f020005;
+        public static final int choumo=0x7f020004;
+        public static final int cilun=0x7f020005;
+        public static final int darkgrey=0x7f02000a;
+        public static final int gongwenbao=0x7f020006;
+        public static final int green=0x7f020011;
+        public static final int ic_launcher=0x7f020007;
+        public static final int maozi=0x7f020008;
+        public static final int mblue=0x7f02000f;
+        public static final int mi=0x7f02000d;
+        public static final int sblue=0x7f02000e;
+        public static final int tgreen=0x7f020012;
+        public static final int white=0x7f020009;
     }
     public static final class id {
-        public static final int adminName=0x7f070002;
-        public static final int archivesNum=0x7f070004;
-        public static final int archivesType=0x7f070003;
-        public static final int commitBtn=0x7f070009;
-        public static final int counterInit=0x7f070005;
-        public static final int counterOver=0x7f070006;
-        public static final int custName=0x7f070001;
-        public static final int memo=0x7f070008;
-        public static final int paperScrap=0x7f070007;
+        public static final int archivesNum=0x7f070001;
+        public static final int commitBtn=0x7f070006;
+        public static final int counterInit=0x7f070002;
+        public static final int counterOver=0x7f070003;
+        public static final int icon=0x7f070008;
+        public static final int memo=0x7f070005;
+        public static final int paperScrap=0x7f070004;
         public static final int progressBar1=0x7f070000;
-        public static final int saveBtn=0x7f07000a;
+        public static final int saveBtn=0x7f070007;
     }
     public static final class layout {
         public static final int activity_first=0x7f030000;
--- PrinterStat/src/com/belstar/printerstat/StatSelView.java
+++ PrinterStat/src/com/belstar/printerstat/StatSelView.java
@@ -1,24 +1,35 @@
 package com.belstar.printerstat;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class StatSelView extends LinearLayout {
-	
+
 	private LayoutInflater mInflater = null;
 	private View sel_item;
-	
-	public StatSelView(Context context) {
-		super(context);
-		
+	private TextView tvTitle;
+	private TextView tvSel;
+	private ImageView ivIcon;
+
+	public StatSelView(Context context, AttributeSet attrs) {
+		super(context, attrs);
 		mInflater = LayoutInflater.from(context);
 		sel_item = mInflater.inflate(R.layout.sel_item, null);
+		tvTitle = (TextView) sel_item.findViewWithTag("tv_title");
+		tvSel = (TextView) sel_item.findViewWithTag("tv_sel");
+		ivIcon = (ImageView) sel_item.findViewWithTag("iv_icon");
+		String tag = this.getTag().toString();
+		String []tags = tag.split("|");
+		tvTitle.setText(tags[0]);
+		tvTitle.setText(tags[0]);
+
 		this.addView(sel_item);
-		
-		
-		
 	}
 
 }
